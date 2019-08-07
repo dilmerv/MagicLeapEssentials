fc990ca9030e92d8bf74549499b520fc8cbe6545
Dilmer Valecillos <dilmervst@gmail.com>
2019-08-07 01:20:31 -0600

    Added Particle System ML Spatial Mapper example

---
+++ Assets/Scripts/MLMeshListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.MagicLeap;
+
+[RequireComponent(typeof(MLSpatialMapper))]
+public class MLMeshListener : MonoBehaviour
+{
+    private MLSpatialMapper _mlSpatialMapper;
+
+    [SerializeField]
+    private ParticleSystem collidingParticleSystem;
+
+    void Start()
+    {
+        _mlSpatialMapper = GetComponent<MLSpatialMapper>();
+        _mlSpatialMapper.meshAdded += MeshAdded;
+    }
+
+    void MeshAdded(TrackableId trackableId)
+    {
+        GameObject gameObject = _mlSpatialMapper.meshIdToGameObjectMap[trackableId];
+        Collider collider = gameObject.GetComponent<Collider>();
+    }
+
+    void OnDestroy()
+    {
+        _mlSpatialMapper.meshAdded -= MeshAdded;
+    }
+}
