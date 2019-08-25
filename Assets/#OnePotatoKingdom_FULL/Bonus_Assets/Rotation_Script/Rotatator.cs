167a9f5462ad2dc7bff96097a8a6580ec75008a2
Dilmer Valecillos <dilmervst@gmail.com>
2019-08-25 15:51:21 -0600

    Added planets scene

---
+++ Assets/#OnePotatoKingdom_FULL/Bonus_Assets/Rotation_Script/Rotatator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotatator : MonoBehaviour {
+	[SerializeField] Vector3 rotation;
+	[SerializeField] Transform meshObject = null;
+	[SerializeField] float rotationSpeed = 0;
+	[SerializeField] bool randomize;
+	
+	public bool Randomize 
+	
+	{
+		get {
+			return randomize;
+		}
+	}
+	
+	[SerializeField] float maxSpeed;
+	[SerializeField] float minSpeed;
+
+	// Use this for initialization
+	void Start () 
+	
+		{
+
+		if(meshObject == null) 
+		
+		{
+			meshObject = transform.Find("planet"); 
+			if (meshObject == null)
+				meshObject = transform.Find("w2"); 
+		}
+		
+		
+		if(randomize) 
+		
+		{
+			rotation = new Vector3(RandFloat(), RandFloat(), RandFloat());
+			rotationSpeed = Random.Range(minSpeed,maxSpeed);
+		}
+	}
+	
+	float RandFloat() 
+	
+	{
+		return Random.Range(0f,1.01f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() 
+	
+	{
+        if(meshObject != null)
+		    meshObject.Rotate(rotation, rotationSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Transitions/Rotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotationSpeed = Vector3.zero;
+
+    void Update() => transform.Rotate(rotationSpeed * Time.deltaTime, Space.Self);
+}
